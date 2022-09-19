876620feac1a6256d671a77f6af9b4e9ec9ec022
Lars Kellogg-Stedman <lars@oddbit.com>
2022-09-19 15:37:25 -0400

    Add support for base64 decoding values

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"flag"
 	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
 
@@ -13,11 +15,15 @@ import (
 var (
 	destDir          *string
 	includeEmptyKeys *bool
+	verbose          *bool
+	base64decode     *bool
 )
 
 func init() {
 	destDir = flag.String("d", ".", "destination directory")
 	includeEmptyKeys = flag.Bool("z", false, "include empty keys")
+	verbose = flag.Bool("v", false, "log filenames")
+	base64decode = flag.Bool("b", false, "base64 decode content before writing")
 }
 
 func contains(haystack []string, needle string) bool {
@@ -40,15 +46,38 @@ func main() {
 		panic(err)
 	}
 
-	err = json.Unmarshal(bytes, &cm)
+	if err := json.Unmarshal(bytes, &cm); err != nil {
+		panic(err)
+	}
+
+	if cm.Kind == "Secret" {
+		log.Printf("will base64 decode values for secrets")
+		*base64decode = true
+	}
+
 	for k, v := range cm.Data {
 		if len(selected) > 0 && !contains(selected, k) {
 			continue
 		}
 
 		if *includeEmptyKeys || v != "" {
+			var buf []byte
+			var err error
+
 			path := filepath.Join(*destDir, k)
-			if err := os.WriteFile(path, []byte(v), 0644); err != nil {
+			if *verbose {
+				log.Printf("writing %s", path)
+			}
+			if *base64decode {
+				buf, err = base64.StdEncoding.DecodeString(v)
+				if err != nil {
+					panic(err)
+				}
+			} else {
+				buf = []byte(v)
+			}
+
+			if err := os.WriteFile(path, buf, 0644); err != nil {
 				panic(err)
 			}
 		}
